b140ce895a2ce1914ac0220e88fdeb94936d1a25
Boyi Ma <boyima98@gmail.com>
2020-11-24 16:29:50 -0500

    Update ex2.sql

--- assignment3/sql/ex2.sql
+++ assignment3/sql/ex2.sql
@@ -54,8 +54,8 @@ CREATE TABLE FlightSchedule (
     arrivalTime TIME NOT NULL,
     seatPrice DECIMAL(10,2) NOT NULL,
     PRIMARY KEY (flightNo),
-    FOREIGN KEY (departureAirportIATA) REFERENCES Route (airportIATA),
-	FOREIGN KEY (arrivalAirportIATA) REFERENCES Route (airportIATA),
+    FOREIGN KEY (departureAirportIATA) REFERENCES Route (departureAirportIATA),
+	FOREIGN KEY (arrivalAirportIATA) REFERENCES Route (arrivalAirportIATA),
     FOREIGN KEY (airlineIATA) REFERENCES Airline(airlineIATA)
 	);
 
