2e1d7401e0e78e2b199e87f56fe70240a3976c31
Boyi Ma <boyima98@gmail.com>
2020-11-28 15:01:28 -0500

    initial commit - connection to server is ok

--- assignment4/src/app.js
+++ assignment4/src/app.js
@@ -1,42 +1,52 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+// set up the project
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const path = require('path');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
 
-var app = express();
-const mysql = require('mysql')
+const port = 5000;
+// const {getHomePage} = require('./routes/index');
+// const {addPlayerPage, addPlayer, deletePlayer, editPlayer, editPlayerPage} = require('./routes/player');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+// Create connection
+const db = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'root',
+  database : 'airline-ticket-database'
+});
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+// Connect to database
+db.connect((err) => {
+  if(err){
+    throw err;
+  }
+  console.log('MySQL Connected...');
+});
+
+//make sure db can be used globally
+global.db = db;
+
+// Configure the middleware
+app.set('port', process.env.port || port); //set port number express uses
+app.set('views', __dirname + '/views'); //set views folder for rendering
+app.set('view engine', 'ejs'); //use ejs as template editor
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json()); //parse from data client
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//connect routes and views
+// app.get('/', getHomePage); //comment missing
+// app.get('/add', addPlayerPage); //comment missing
+// app.get('/edit/:id', editPlayerPage); //comment missing
+// app.get('/delete/:id', deletePlayer); //comment missing
+// app.post('/add', addPlayer); //comment missing
+// app.post('/edit/:id', editPlayer); //comment missing
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+
+// set the app to listen on the port
+app.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
 });
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
